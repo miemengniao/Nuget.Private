890b6cb4889bea3de25b78e62d2fd51376fdde2d
MieMengBird <964635480@qq.com>
2017-06-17 12:11:47 +0800

    Nuget服务器示例代码

---
+++ Nuget.Private/PackageSample/Readme.txt
@@ -0,0 +1,5 @@
+﻿
+1.  启动运行Nuget.Private网站
+2.  在PackageSample工程目录下，运行Nuget pack PackageSample.nuspec  生成PackageSample.0.0.2.nupkg包
+3.  在PackageSample工程目录下，运行Nuget push PackageSample.0.0.2.nupkg Please.Change.To.Your.Nuget.Private.Key  -Source http://localhost:55545/nuget
+4.  在c:\StorageForNugetPackage目录可以看到PackageSample已经上传了。也就是你可以引用自己的Nuget服务器来维护私有包了
---
+++ Nuget.Private/PackageSample/SampleClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageSample
+{
+    /// <summary>
+    /// 用于展示package上传功能的类
+    /// </summary>
+    public class SampleClass
+    {
+        /// <summary>
+        /// 用于展示package上传功能的方法
+        /// </summary>
+        public void SampleMethod()
+        {
+
+        }
+    }
+}
